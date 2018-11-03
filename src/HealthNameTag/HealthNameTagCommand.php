88d4ab6eed4ca2c81cc0e301e86013de31a8bb80
WarTechDevs <44534195+WarTechDevs@users.noreply.github.com>
2018-11-02 21:43:40 -0400

    Update HealthNameTagCommand.php

--- src/HealthNameTag/HealthNameTagCommand.php
+++ src/HealthNameTag/HealthNameTagCommand.php
@@ -1,13 +1,8 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: War Tech
  * Date: 11/2/2018
  * Time: 7:52 PM
- * this isnt yet done
- * things todo listed below
- * show health in chat using purechat and pureperms
- * add support for \n
- * add to show rank on name tag basically add support for purechat and pureperms
- * add a healthnametag command to enable and disable for OPS only
  */

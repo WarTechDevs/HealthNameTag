06210e1f23de910e788d345dc98e76662c3e72c1
WarTechDevs <44534195+WarTechDevs@users.noreply.github.com>
2018-11-02 21:10:53 -0400

    fix 1 problem?

--- src/HealthNameTag/HealthNameTagTask.php
+++ src/HealthNameTag/HealthNameTagTask.php
@@ -1,31 +1,22 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: War Tech aka Johnathen
- * Date: 11/2/2018
- * Time: 7:13 PM
- */
-
 namespace HealthNameTag;
-
-use pocketmine\Server{Server, Player};
 
 use pocketmine\Plugin;
 
+use pocketmine\{Server, Player};
+
 use pocketmine\scheduler\Task;
 
-class HealthNameTag extends Task
-
-    public function __construct($plugin) {
-
+class HealthNameTag extends Task {
+    public function __construct($plugin){
         $this->plugin = $plugin;
     }
 
-    public function onRun(currentTick) {
-        foreach ($this->plugin->getServer()->getOnlinePlayers() as $p) {
+    public function onRun($currentTick){
+        foreach($this->plugin->getServer()->getOnlinePlayers() as $p){
             $player = $p;
-            $p->getScoreTag("Players Health " "$player->getHealth () / $player->getmaxHealth() * 230 )");
+            $p->setScoreTag("Players Health "($player->getHealth() / $player->getMaxHealth() * 20));
         }
     }
 }
